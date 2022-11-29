89c14a0a86cbd531dc2a7905acdd73f9f242964a
manish <manishpanwar682@gmail.com>
2022-11-29 01:27:03 -1200

    hello

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var cors = require('cors')
 app.use(express.json());
 app.use(cors()) 
 app.use('/',todolist);
+app.get("/",(req,res)=>{
+res.send("hello");
+})
 const port = process.env.PORT || 5000
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
