ec0ffe4d9778aacefcfaa875a415c6e71804f544
manish <manishpanwar682@gmail.com>
2022-11-29 17:14:36 -1200

    hello

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const database = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("database connected");
   } catch (err) {
     console.log(err.message);
